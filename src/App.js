d75b0b3132b8c1a8153f41712bab38805d672784
Montasir Azad <montasirazad@gmail.com>
2023-11-04 03:30:53 +0600

    repo ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.css';
+import { mainData } from './Data/dataMain';
+
+
 
 function App() {
+
+
+  const [allData, setAllData] = useState(mainData)
+  const [cart, setCart] = useState([]);
+
+  let dragItem = useRef();
+  let dragOverItem = useRef()
+
+  const startDrag = (index) => {
+    dragItem.current = index
+    //console.log(dragItem);
+  };
+
+  const dragEnter = (index) => {
+    dragOverItem.current = index;
+
+  }
+  const handleDragEnd = () => {
+    const copiedArray = [...allData];
+    const dragItemContent = copiedArray[dragItem.current];
+    copiedArray.splice(dragItem.current, 1)
+    copiedArray.splice(dragOverItem.current, 0, dragItemContent)
+    dragItem.current = null;
+    dragOverItem.current = null;
+    setAllData(copiedArray)
+    //console.log('drag item content', dragItemContent);
+  }
+
+
+
+
+  const handleCheck = (e) => {
+    const isChecked = e.target.checked;
+    const itemId = e.target.value;
+    if (isChecked) {
+      const newArray = [...cart, itemId];
+      setCart(newArray)
+
+      console.log(e.target.value);
+    } if (!isChecked) {
+      const updatedCart = cart.filter(item => item !== itemId)
+      setCart(updatedCart)
+    }
+
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h3>Hello</h3>
+
+      {cart.length ? <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
+        <h2>item added:{cart.length}</h2>
+        <button onClick={() => { window.location.reload() }}>Delete items</button>
+      </div> : null}
+
+      <div style={{ display: 'flex', flexWrap: 'wrap', transition: 'all 1s ease-in-out' }}>
+
+        {
+          allData.map((data, index) =>
+          (
+            <div
+              ref={dragOverItem}
+              style={index === 0 ? { width: '180px', flexGrow: '3', height: '180px', border: '1px solid green', margin: '10px', cursor: 'move', transition: 'all .2s ease-in-out' } :
+                { width: '80px', height: '80px', border: '1px solid red', margin: '10px', cursor: 'move', transition: 'all .2s ease-in-out' }
+              }
+              key={data.id}
+              draggable
+
+              onDragStart={() => startDrag(index)}
+              onDragEnter={() => dragEnter(index)}
+              onDragEnd={() => handleDragEnd()}
+            >
+              <label style={{ cursor: 'pointer' }}>
+                <input type="checkbox" name="" id="" value={data.id} onClick={(e) => handleCheck(e, data)} />Select
+              </label>
+              <img src={data.image} alt="" style={index === 0 ? { width: '140px', height: '140px' } : { width: '50px', height: '50px' }} />
+            </div>)
+          )
+        }
+      </div>
     </div>
   );
 }
---
+++ src/Data/dataMain.js
@@ -0,0 +1,61 @@
+import imgOne from '../images/image-1.webp';
+import imgTwo from '../images/image-2.webp';
+import imgThree from '../images/image-3.webp';
+import imgFour from '../images/image-4.webp';
+import imgFive from '../images/image-5.webp';
+import imgSix from '../images/image-6.webp';
+import imgSeven from '../images/image-7.webp';
+import imgEight from '../images/image-8.webp';
+import imgNine from '../images/image-9.webp';
+import imgTen from '../images/image-10.jpeg';
+import imgEleven from '../images/image-11.jpeg';
+
+export const mainData = [
+    {
+        id: 1,
+        name: 'One',
+        image: imgOne
+    },
+    {
+        id: 2,
+        name: 'two',
+        image: imgTwo
+    }, {
+        id: 3,
+        name: 'three',
+        image: imgThree
+    }, {
+        id: 4,
+        name: 'four',
+        image: imgFour
+    }, {
+        id: 5,
+        name: 'five',
+        image: imgFive
+    }, {
+        id: 6,
+        name: 'six',
+        image: imgSix
+    }, {
+        id: 7,
+        name: 'seven',
+        image: imgSeven
+    }, {
+        id: 8,
+        name: 'eight',
+        image: imgEight
+    }, {
+        id: 9,
+        name: 'nine',
+        image: imgNine
+    }, {
+        id: 10,
+        name: 'ten',
+        image: imgTen
+    }
+    , {
+        id: 11,
+        name: 'eleven',
+        image: imgEleven
+    }
+];
